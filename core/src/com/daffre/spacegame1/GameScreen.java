7198cd246f2d0085d1423711a0aa149a8a49d022
Gianluca Daffré <gianluca.daffre@noseryoung.ch>
2020-06-10 13:12:11 +0200

    added some stuff

--- core/src/com/daffre/spacegame1/GameScreen.java
+++ core/src/com/daffre/spacegame1/GameScreen.java
@@ -1,19 +1,39 @@
 package com.daffre.spacegame1;
 
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class GameScreen implements Screen {
 
 
+    private SpriteBatch batch;
+    private Sprite ship;
+    private final Texture img;
 
-    @Override
-    public void show() {
+    public GameScreen(Game parent) {
+        batch = new SpriteBatch();
+        img = new Texture("fighter.png");
+        ship = new Sprite(img);
 
     }
 
     @Override
-    public void render(float delta) {
+    public void show() {
+    }
 
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        batch.begin();
+        ship.draw(batch);
+        batch.end();
     }
 
     @Override
@@ -38,6 +58,8 @@ public class GameScreen implements Screen {
 
     @Override
     public void dispose() {
+        img.dispose();
+        batch.dispose();
 
     }
 }
--- core/src/com/daffre/spacegame1/MainMenuScreen.java
+++ core/src/com/daffre/spacegame1/MainMenuScreen.java
@@ -54,7 +54,7 @@ public class MainMenuScreen implements Screen {
         playButton.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
-                //parent.setScreen(new GameScreen(parent));
+                parent.setScreen(new GameScreen(parent));
             }
         });
         exitButton.addListener(new ClickListener(){
